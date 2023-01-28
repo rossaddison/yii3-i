373ab263d3124ddf3b19a6754467993322f4d6ca
Ross Addison <rossaddison@users.noreply.github.com>
2023-01-28 16:44:36 +0000

    Avoid Psalm Level 3: PossiblyNullReference on OffsetPagination
    Null Safe Operator ensures that $this->paginator is not a Null Reference and therefore the subsequent or next method can be called.

--- src/Widget/FlashMessage.php
+++ src/Widget/FlashMessage.php
@@ -1,5 +1,4 @@
 <?php
-
 declare(strict_types=1);
 
 namespace App\Widget;
@@ -7,13 +6,12 @@ namespace App\Widget;
 use Yiisoft\Session\Flash\FlashInterface;
 use Yiisoft\Widget\Widget;
 use Yiisoft\Yii\Bootstrap5\Alert;
-
 final class FlashMessage extends Widget
 {
     public function __construct(private FlashInterface $flash)
     {
     }
-
+    
     public function render(): string
     {
         $flashes = $this->flash->getAll();
@@ -26,7 +24,6 @@ final class FlashMessage extends Widget
                     ->body($message['body']);
             }
         }
-
         return implode($html);
     }
 }
